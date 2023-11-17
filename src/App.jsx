c1037d3611f106fe718d60aeb6f14235db18e270
Gabriel Raiol <raiol.gabriel@hotmail.com>
2023-11-17 17:32:02 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Post } from "./components/Post";
+
+export function App() {
+
+  return(
+    <Post
+        author="Gabriel Raiol"
+        content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia delectus quasi fuga, vitae consectetur nobis, magni voluptates praesentium saepe id minima sequi sapiente accusamus? Alias cum ad illo ab vero!"
+    />
+  )
+
+}
