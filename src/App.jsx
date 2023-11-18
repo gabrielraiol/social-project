fad4a1c668bcf75432ca0435b3e898c59d979895
Gabriel Raiol <raiol.gabriel@hotmail.com>
2023-11-17 22:51:01 -0300

    initial changes; absolute import; saas; global variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
+import { Header } from "./components/Header";
 import { Post } from "./components/Post";
+import "./global.css";
 
 export function App() {
 
   return(
-    <Post
-        author="Gabriel Raiol"
-        content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia delectus quasi fuga, vitae consectetur nobis, magni voluptates praesentium saepe id minima sequi sapiente accusamus? Alias cum ad illo ab vero!"
-    />
+    <div>
+        <Header/>
+
+        <Post
+            author="Gabriel Raiol"
+            content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia delectus quasi fuga, vitae consectetur nobis, magni voluptates praesentium saepe id minima sequi sapiente accusamus? Alias cum ad illo ab vero!"
+        />
+    </div>
   )
 
 }
