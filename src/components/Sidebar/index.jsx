b9128c079dfa2252f809dfbe6aee3f27be4d32f2
Gabriel Raiol <raiol.gabriel@hotmail.com>
2023-11-21 16:25:59 -0300

    finishing sidebar

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,4 +1,5 @@
 import styles from "./Sidebar.module.scss";
+import {PencilSimpleLine} from 'phosphor-react';
 
 export function Sidebar() {
     return(
@@ -6,16 +7,24 @@ export function Sidebar() {
             <img 
                 className={styles.cover}
                 src="https://plus.unsplash.com/premium_photo-1663023612721-e588768ef403?q=50&w=600&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                alt="Profile Picture"
+                alt="Cover Picture"
             />
 
             <div className={styles.profile}>
+                <img
+                    className={styles.profile__avatar}
+                    src="https://avatars.githubusercontent.com/u/39103851?v=4"
+                    alt="Profile Picture"
+                />
                 <strong>Gabriel Raiol</strong>
                 <span>Web Developer</span>
             </div>
 
             <footer>
-                <a href="#">Editar seu Perfil</a>
+                <a href="#">
+                    <PencilSimpleLine size={20}/>
+                    Editar seu Perfil
+                </a>
             </footer>
         </aside>
     )
