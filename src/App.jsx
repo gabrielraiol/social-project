8c6b10b68b7753c1065cef01cf4133667bbd3fc1
Gabriel Raiol <raiol.gabriel@hotmail.com>
2023-11-22 18:35:00 -0300

    adding styles to post component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { Header } from "./components/Header";
 import { Post } from "./components/Post";
+import { Sidebar } from "./components/Sidebar";
 import "./global.css";
 import styles from "./App.module.scss";
-import { Sidebar } from "./components/Sidebar";
 
 export function App() {
 
@@ -17,6 +17,8 @@ export function App() {
                 author="Gabriel Raiol"
                 content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia delectus quasi fuga, vitae consectetur nobis, magni voluptates praesentium saepe id minima sequi sapiente accusamus? Alias cum ad illo ab vero!"
             />
+
+            <Post/>
           </main>
         </div>
     </div>
--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -1,8 +1,34 @@
-export function Post(props) {
+import styles from "./Post.module.scss";
+
+export function Post() {
     return(
-        <div>
-            <h1>{props.author}</h1>
-            <p>{props.content}</p>
-        </div>
+        <article className={styles.post}>
+            <header>
+                <div className={styles.author}>
+                    <img
+                        className={styles.author__avatar}
+                        src="https://avatars.githubusercontent.com/u/39103851?v=4"
+                        alt="Profile Picture"
+                    />
+                    <div className={styles.author__info}>
+                        <strong>Gabriel Raiol</strong>
+                        <span>Web Developer</span>
+                    </div>
+                </div>
+
+                <time title="23 de Novembro de 2023 às 16h47min" dateTime="2023-11-21 16:47:04">Publicado há 1h</time>
+            </header>
+
+            <div className={styles.content}>
+                <p>Faaala galera 👋</p>
+                <p>Acabei de subir mais um projeto no meu portfolio. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+                <p>👉{' '}<a href="#">https://github.com/gabrielraiol</a></p>
+                <p>
+                    <a href="#">#novoprojeto</a>{' '}
+                    <a href="#">#nlw</a>{' '}
+                    <a href="#">#rocketseat</a>
+                </p>
+            </div>
+        </article>
     )
 }
